58b274fdf6538d47c95a6ff364881f75e9015e5e
TamiPines <64363390+TamiPines@users.noreply.github.com>
2021-01-20 19:19:59 +0200

    20/1

--- Mazal-Tov WebApi/BL/CandidateBL.cs
+++ Mazal-Tov WebApi/BL/CandidateBL.cs
@@ -9,7 +9,7 @@ using System.Threading.Tasks;
 
 namespace BL
 {
-   public class CandidateBL
+    public class CandidateBL
     {
         public static DAL.MazalTovEntities db = new DAL.MazalTovEntities();
         public static Candidate RegisterCandidate(Candidate candidate)
@@ -17,8 +17,8 @@ namespace BL
             try
             {
                 candidate.User.Roles = new List<Role>();
-                var role=db.roles.First(p => p.Name == "User");
-                var roleDto= Converters.Convert(role);
+                var role = db.roles.First(p => p.Name == "User");
+                var roleDto = Converters.Convert(role);
                 candidate.User.Roles.Add(roleDto);
                 var user = UserBL.Register(candidate.User);
                 candidate.UserId = user.Id;
@@ -26,7 +26,7 @@ namespace BL
                 candidate.UpdateDate = DateTime.Now;
                 var candidateDB = Converters.Convert(candidate);
                 var candidateInter = db.candidates.Add(candidateDB);
-                
+
                 db.SaveChanges();
                 return Converters.Convert(candidateInter);
             }
@@ -45,5 +45,39 @@ namespace BL
             }
             return AllCandidate;
         }
+
+
+
+        public static List<Candidate> GetAllSearchCandidate(Candidate candidate)
+        {
+            List<DTO.Candidate> list = new List<Candidate>();
+            foreach (var item in db.candidates)
+            {
+                list.Add(Converters.Convert(item));
+            }
+            
+            if (candidate.SectorId != null)
+                list = list.Where(r => r.SectorId == candidate.SectorId).ToList();
+            if (candidate.Gender != null)
+                list = list.Where(r => r.Gender == candidate.Gender).ToList();
+            if (candidate.Status != null)
+                list = list.Where(r => r.Status == candidate.Status).ToList();
+            if (candidate.Clothes != null)
+                list = list.Where(r => r.Clothes == candidate.Clothes).ToList();
+            if (candidate.MedicalProblem != null)
+                list = list.Where(r => r.MedicalProblem == candidate.MedicalProblem).ToList();
+            return list;
+        }
     }
+
 }
+
+
+
+
+//if (candidate.CityId != null && SearchDate2 != null)
+//    list = list.Where(r => r.Date.Value.Date >= SearchDate1.Date && r.Date.Value.Date <= SearchDate2.Date).ToList();
+//if (SearchDate1 != null && SearchDate2 == null)
+//    list = list.Where(r => r.Date >= SearchDate1).ToList();
+//if (SearchDate2 != null && SearchDate1 == null)
+//    list = list.Where(r => r.Date <= SearchDate2).ToList();
--- Mazal-Tov WebApi/MazalTovApi/Controllers/HelperController.cs
+++ Mazal-Tov WebApi/MazalTovApi/Controllers/HelperController.cs
@@ -21,21 +21,21 @@ namespace MazalTovApi.Controllers
             return HelperBL.RegisterHelper(helper);
         }
 
-<<<<<<< HEAD
-        //[HttpGet]
-        //[Authorize(Roles = "Worker")]
-        //[Route("GetMatchingHelper")]
-=======
-        [HttpGet]
-        [Authorize(Roles = "Worker,Admin")]
-        [Route("GetMatchingHelper")]
->>>>>>> 7dfe90d7e957da7299624686156841a4dfd1bbc8
+//<<<<<<< HEAD
+//        [HttpGet]
+//        [Authorize(Roles = "Worker")]
+//        [Route("GetMatchingHelper")]
+////=======
+//        [HttpGet]
+//        [Authorize(Roles = "Worker,Admin")]
+//        [Route("GetMatchingHelper")]
+//>>>>>>> 7dfe90d7e957da7299624686156841a4dfd1bbc8
   
-        //public List<DTO.MatchingHelper> GetAllMatchingHelper()
-        //{
-        //    var identity = (ClaimsIdentity)User.Identity;
-        //    return HelperBL.GetAllMatching(int.Parse( identity.Name));
-        //}
+//        //public List<DTO.MatchingHelper> GetAllMatchingHelper()
+//        //{
+//        //    var identity = (ClaimsIdentity)User.Identity;
+//        //    return HelperBL.GetAllMatching(int.Parse( identity.Name));
+//        //}
 
         [HttpGet]
        
